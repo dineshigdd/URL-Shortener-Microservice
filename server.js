b1d6ff847d05cc863b8ed17ec5b50c716c56d597
Glitch (excessive-octopus) <none>
2019-08-30 05:24:13 +0000

    🎻🏤 Checkpoint ./server.js:6079345/1746

--- server.js
+++ server.js
@@ -41,29 +41,35 @@ app.post("/api/shorturl/new", function (req, res) {
   var shorturl = 0;
   shorturl = Math.floor(Math.random() * 10000);
 
- // URL.remove({originalURL: req.body.url},(err,data) =>{
+ // URL.remove({},(err,data) =>{
  //    err?res.send(err):res.send("deleted")
  //  })
-//   var url = new URL( { originalURL: req.body.url,shortURL:shorturl });
-//               url.save((err,data)=>{
-//                     if(err){
-//                        return console.log(err);
-//                     }else{
-//                      res.send("saved to DB");
-                     
-//                     }
-//               });
+
    URL.count((err,count)=>{
            
           if( count == 0) {
-              res.send("No Found Records.");
+             // res.send("No Found Records.");
+              var url = new URL( { originalURL: req.body.url,shortURL:shorturl });
+              url.save((err,data)=>{
+                    if(err){
+                       return console.log(err);
+                    }else{
+                     res.send("saved to DB");
+                     
+                    }
+              });
             
           }
           else {
             
-             res.send("Found Records : " + count);
+             //res.send("Found Records : " + count);
              URL.find({ originalURL: req.body.url}, (err, data)=>{
-               console.log(data);
+                if( data[0].originalURL === req.body.url ){
+                   res.json({
+                    original_url: req.body.url,
+                    short_url: shorturl
+                   })
+                }
              });
               
           }
